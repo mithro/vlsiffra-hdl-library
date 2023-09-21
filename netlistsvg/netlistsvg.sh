7eccfd254ab3432cf695bcb8f2a481f4a1852605
Tim 'mithro' Ansell <tansell@google.com>
2023-09-21 15:07:35 -0700

    Get netlistsvg from conda for nicer diagrams.
    Signed-off-by: Tim 'mithro' Ansell <tansell@google.com>

---
+++ netlistsvg/netlistsvg.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+SOURCE=${BASH_SOURCE[0]}
+while [ -L "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+  SOURCE=$(readlink "$SOURCE")
+  [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+
+set -x
+set -e
+
+cd $DIR
+
+make version
+source ./env/conda/bin/activate netlistsvg
+exec netlistsvg "$@"
